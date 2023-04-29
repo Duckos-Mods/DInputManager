3e7961658e7498a805613f8d135076f95a3aeca7
Duckos-Mods <107327509+Duckos-Mods@users.noreply.github.com>
2023-04-29 09:07:01 +0100

    Add files via upload

---
+++ keyboardManager.cpp
@@ -0,0 +1,65 @@
+#include "keyboardManager.h"
+#include "..\raylib.h"
+
+void KeyboardManager::AddKey(int Key, std::function<void()> Function, Mode KeyMode)
+{
+    auto WrapperFunc = [Function]() { Function(); };
+    _keyFunctions.try_emplace(Key, WrapperFunc);
+    KeyMapData Data = { false, KeyMode };
+    _keyData.try_emplace(Key, Data);
+    _keys.push_back(Key);
+}
+
+
+void KeyboardManager::AddMouseButton(int Key, std::function<void()> Function, Mode KeyMode)
+{
+    auto WrapperFunc = [Function]() { Function(); };
+    _mouseFunctions.try_emplace(Key, WrapperFunc);
+    MouseMapData Data = { false, KeyMode };
+    _mouseData.try_emplace(Key, Data);
+    _miceButtons.push_back(Key);
+}
+
+void KeyboardManager::Update()
+{
+    for (auto& CurrentKey : _keys)
+    {
+        if (_keyData[CurrentKey].mode == Hold)
+        {
+            if (IsKeyDown(CurrentKey))
+            {
+                _keyFunctions[CurrentKey]();
+                _keyData[CurrentKey].down = true;
+            }
+        }
+        else if (_keyData[CurrentKey].mode == Press)
+        {
+            if (IsKeyPressed(CurrentKey))
+            {
+                _keyFunctions[CurrentKey]();
+                _keyData[CurrentKey].down = true;
+            }
+        }
+    }
+
+    for (auto& CurrentMouseKey : _miceButtons)
+    {
+        if (_mouseData[CurrentMouseKey].mode == Hold)
+        {
+            if (IsMouseButtonDown(CurrentMouseKey))
+            {
+                _mouseFunctions[CurrentMouseKey]();
+                _mouseData[CurrentMouseKey].down = true;
+            }
+        }
+        else if (_mouseData[CurrentMouseKey].mode == Press)
+        {
+            if (IsMouseButtonPressed(CurrentMouseKey))
+            {
+                _mouseFunctions[CurrentMouseKey]();
+                _mouseData[CurrentMouseKey].down = true;
+            }
+        }
+    }
+}
+
---
+++ keyboardManager.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <functional>
+#include <map>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <array>
+
+enum Mode
+{
+    Hold,
+    Toggle,
+    Press
+};
+
+struct KeyMapData
+{
+    bool down = false;
+    Mode mode = Hold;
+};
+
+struct MouseMapData
+{
+
+    bool down = false;
+    Mode mode = Hold;
+};
+
+class KeyboardManager 
+{
+public:
+    template <typename... Args>
+    void AddKey(int Key, std::function<void(Args...)> Function, Mode mode,Args... args)
+    {
+        auto WrapperFunc = [Function, args...]() { Function(args...); };
+        _keyFunctions.try_emplace(Key, WrapperFunc);
+        KeyMapData Data = { false, mode };
+        _keyData.try_emplace(Key, Data);
+        _keys.push_back(Key);
+
+    }
+    void AddKey(int Key, std::function<void()> Function, Mode KeyMode);
+
+
+    template <typename... Args>
+    void AddMouseButton(int Key, std::function<void(Args...)> Function, Mode mode, Args... args)
+    {
+        auto WrapperFunc = [Function, args...]() { Function(args...); };
+        _mouseFunctions.try_emplace(Key, WrapperFunc);
+        MouseMapData Data = { false, mode };
+        _mouseData.try_emplace(Key, Data);
+        _miceButtons.push_back(Key);
+    }
+
+
+    void AddMouseButton(int Key, std::function<void()> Function, Mode KeyMode);
+
+    void Update();
+private:
+    std::map <int, std::function<void()>> _keyFunctions;
+    std::map<int, KeyMapData> _keyData;
+    std::vector<int> _keys;
+
+    std::map<int, std::function<void()>> _mouseFunctions;
+    std::map<int, MouseMapData> _mouseData;
+    std::vector<int> _miceButtons;
+};
+
